e291197d2c545a77917ade36998b3cdc44107f18
ericlndbrg <ericrlindberg@gmail.com>
2021-05-05 19:58:59 -0700

    added net/http functionality

--- app.rb
+++ app.rb
@@ -2,10 +2,18 @@
 
 require 'dotenv/load'
 require 'net/http'
+require 'json'
 
 Dotenv.require_keys('API_KEY')
 
 uri = URI('https://api.nasa.gov/planetary/apod')
 params = {api_key: ENV['API_KEY']}
 uri.query = URI.encode_www_form(params)
-puts Net::HTTP.get(uri)
+
+response = JSON.parse(Net::HTTP.get(uri))
+if response['media_type'] == 'image'
+  puts response['hdurl']
+  puts response['title']
+else
+  puts "The response for #{response['date']} is a #{response['media_type']}, not an image."
+end
