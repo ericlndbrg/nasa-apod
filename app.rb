06efdb5f0e4a387f4b0cec06aa584d9867c0532f
ericlndbrg <ericrlindberg@gmail.com>
2021-05-09 09:01:13 -0700

    organized the code into a class, added a directory for images and added wget functionality

--- app.rb
+++ app.rb
@@ -1,19 +1,45 @@
 #!/usr/bin/env ruby
 
 require 'dotenv/load'
-require 'net/http'
-require 'json'
 
 Dotenv.require_keys('API_KEY')
 
-uri = URI('https://api.nasa.gov/planetary/apod')
-params = {api_key: ENV['API_KEY']}
-uri.query = URI.encode_www_form(params)
+class NasaApodApi
 
-response = JSON.parse(Net::HTTP.get(uri))
-if response['media_type'] == 'image'
-  puts response['hdurl']
-  puts response['title']
-else
-  puts "The response for #{response['date']} is a #{response['media_type']}, not an image."
+  require 'net/http'
+  require 'json'
+  require 'open3'
+  
+  attr_accessor :api_response
+  
+  def get_image_uri
+    make_api_request
+  end
+
+  def download_image(uri, title)
+    stdout, stderr, status = Open3.capture3("wget --output-document=2021-05-09:#{title}.jpg #{uri}")
+  end
+
+  private
+
+  def build_uri
+    uri = URI('https://api.nasa.gov/planetary/apod')
+    required_params = {api_key: ENV['API_KEY']}
+    uri.query = URI.encode_www_form(required_params)
+    uri
+  end
+
+  def make_api_request
+    self.api_response = JSON.parse(Net::HTTP.get(build_uri))
+    if self.api_response['media_type'] == 'image'
+      [self.api_response['hdurl'], self.api_response['title']]
+    else
+      "The response for #{self.api_response['date']} is a #{self.api_response['media_type']}, not an image."
+    end
+  end
+
 end
+
+apod = NasaApodApi.new
+nasa_response = apod.get_image_uri
+apod.download_image(nasa_response[0], nasa_response[1].gsub(' ', '-'))
