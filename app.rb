2890aa3a600c4d0cad063a2092515f21467a2462
ericlndbrg <ericrlindberg@gmail.com>
2022-04-15 08:32:31 -0400

    added APP_ENV environment variable and a check for its validity

--- app.rb
+++ app.rb
@@ -3,8 +3,18 @@
 require_relative 'classes/application'
 
 def main
+  # if APP_ENV hasn't been set yet, set it to 'dev'
+  ENV['APP_ENV'] ||= 'dev'
+  # APP_ENV is only allowed to be 'dev' or 'prod'
+  unless ['dev', 'prod'].include?(ENV['APP_ENV'])
+    raise(StandardError, 'Unpermitted environment')
+  end
+
   app = Application.new
   app.run
+
+rescue StandardError => e
+  puts e.message
 end
 
 main
