1514f6001068f9f113a4396050c6aeeb036ad09f
ericlndbrg <ericrlindberg@gmail.com>
2022-04-15 08:48:58 -0400

    the database used is now dependent on the value of ENV['APP_ENV']

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ require_relative 'classes/application'
 def main
   # if APP_ENV hasn't been set yet, set it to 'dev'
   ENV['APP_ENV'] ||= 'dev'
-  # APP_ENV is only allowed to be 'dev' or 'prod'
+  # for now, ENV['APP_ENV'] is only allowed to be 'dev' or 'prod'
   unless ['dev', 'prod'].include?(ENV['APP_ENV'])
-    raise(StandardError, 'Unpermitted environment')
+    raise(StandardError, 'Please run the app in one of these environments: dev, prod.')
   end
 
   app = Application.new
--- classes/database.rb
+++ classes/database.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 
 class Database
   def execute_query(query, data, return_result: false)
-    db = SQLite3::Database.new('db/dev.db', results_as_hash: true)
+    db = SQLite3::Database.new("db/#{ENV['APP_ENV']}.db", results_as_hash: true)
     result = db.execute(query, data)
     db.close
     return result if return_result
